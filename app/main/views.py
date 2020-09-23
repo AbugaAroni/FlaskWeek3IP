fdbfc6407cc3606d6f45fed81bb06bc1fd5251ef
AbugaAroni <abugaaroni@gmail.com>
2020-09-23 21:08:21 +0300

    add a working view user profile class

--- app/main/views.py
+++ app/main/views.py
@@ -48,6 +48,15 @@ def new_pitch(userid):
     title = 'New Pitch'
     return render_template('new_pitch.html',title = title, pitch_form=form)
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)    
+
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
 def update_profile(uname):
@@ -63,6 +72,6 @@ def update_profile(uname):
         db.session.add(user)
         db.session.commit()
 
-        return redirect(url_for('.profile',uname=user.username))
+        return redirect(url_for('.update_profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
