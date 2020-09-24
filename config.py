110e5d417c7a3836bc17dc0d4a9a303bac012e41
AbugaAroni <abugaaroni@gmail.com>
2020-09-24 14:07:53 +0300

    herokue commit

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://abuga:password@localhost/pitches'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://abuga:password@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     #  email configurations
     MAIL_SERVER = 'smtp.gmail.com'
